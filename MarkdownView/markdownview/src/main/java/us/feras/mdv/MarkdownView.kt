3fe9bd4f23824a319484dff97c7a7911ed333dc8
Minsuk Eom <boxresin@gmail.com>
2020-12-26 00:02:14 +0900

    Rewrite source code from Java to Kotlin

--- MarkdownView/markdownview/src/main/java/us/feras/mdv/MarkdownView.kt
+++ MarkdownView/markdownview/src/main/java/us/feras/mdv/MarkdownView.kt
@@ -1,131 +1,113 @@
-package us.feras.mdv;
+package us.feras.mdv
 
-import android.content.Context;
-import android.content.res.TypedArray;
-import android.util.AttributeSet;
-import android.util.Log;
-import android.webkit.WebView;
-
-import androidx.annotation.StringRes;
-
-import org.markdownj.MarkdownProcessor;
-
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import android.content.Context
+import android.content.res.TypedArray
+import android.util.AttributeSet
+import android.util.Log
+import android.webkit.WebView
+import androidx.annotation.StringRes
+import org.markdownj.MarkdownProcessor
 
 /**
  * @author Feras Alnatsheh
  */
-public class MarkdownView extends WebView {
+class MarkdownView : WebView {
+    private var markdownText: String = ""
+    private var cssText: String = ""
+    private val markdownProcessor = MarkdownProcessor()
 
-	private static final String TAG = "MarkdownView";
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs) {
+        val typedArray: TypedArray = context.obtainStyledAttributes(attrs, R.styleable.MarkdownView)
+        val mdText: String = typedArray.getString(R.styleable.MarkdownView_markdown) ?: ""
+        val mdPath: String = typedArray.getString(R.styleable.MarkdownView_markdownFromAssets) ?: ""
+        val cssText: String = typedArray.getString(R.styleable.MarkdownView_css) ?: ""
+        val cssPath: String = typedArray.getString(R.styleable.MarkdownView_cssFromAssets) ?: ""
 
-	private String markdownText;
-	private String cssText;
-	private final MarkdownProcessor markdownProcessor;
+        if (mdText.isNotEmpty()) {
+            this.loadMarkdown(mdText)
+        } else if (mdPath.isNotEmpty()) {
+            this.loadMarkdownFromAssets(mdPath)
+        }
 
-	public MarkdownView(Context context, AttributeSet attrs) {
-		super(context, attrs);
-		markdownProcessor = new MarkdownProcessor();
+        if (cssText.isNotEmpty()) {
+            this.loadCss(cssText)
+        } else if (cssPath.isNotEmpty()) {
+            this.loadCssFromAssets(cssPath)
+        }
 
-		TypedArray typedArray = getContext().obtainStyledAttributes(attrs, R.styleable.MarkdownView);
+        typedArray.recycle()
+    }
 
-		String markdownText = typedArray.getString(R.styleable.MarkdownView_markdown);
-		String markdownPath = typedArray.getString(R.styleable.MarkdownView_markdownFromAssets);
-		String cssText = typedArray.getString(R.styleable.MarkdownView_css);
-		String cssPath = typedArray.getString(R.styleable.MarkdownView_cssFromAssets);
+    constructor(context: Context) : super(context)
 
-		if (markdownText != null)
-			loadMarkdown(markdownText);
-		else if (markdownPath != null)
-			loadMarkdownFromAssets(markdownPath);
-		if (cssText != null)
-			loadCss(cssText);
-		else if (cssPath != null)
-			loadCssFromAssets(cssPath);
+    /**
+     * Loads the given Markdown text to the view as rich formatted HTML.
+     * @param markdownText input in Markdown format
+     */
+    fun loadMarkdown(markdownText: String) {
+        this.markdownText = markdownText
+        this.syncMarkdown()
+    }
 
-		typedArray.recycle();
-	}
+    /**
+     * Loads the given Markdown text to the view as rich formatted HTML.
+     * @param markdownTextId Markdown text id from String resource.
+     */
+    fun loadMarkdown(@StringRes markdownTextId: Int) {
+        this.loadMarkdown(this.resources.getString(markdownTextId))
+    }
 
-	public MarkdownView(Context context) {
-		super(context);
-		markdownProcessor = new MarkdownProcessor();
-	}
+    /**
+     * Loads the given Markdown text from Android assets.
+     * @param markdownPath Markdown text file in assets directory. (ex. "hello.md")
+     */
+    fun loadMarkdownFromAssets(markdownPath: String) {
+        this.loadMarkdown(this.readFileFromAsset(markdownPath))
+    }
 
-	/**
-	 * Loads the given Markdown text to the view as rich formatted HTML.
-	 * @param markdownText input in Markdown format
-	 */
-	public void loadMarkdown(String markdownText)
-	{
-		this.markdownText = markdownText;
-		syncMarkdown();
-	}
+    /**
+     * Loads the given CSS text to the Markdown text.
+     * @param cssText input in CSS format
+     */
+    fun loadCss(cssText: String) {
+        this.cssText = cssText
+        this.syncMarkdown()
+    }
 
-	/**
-	 * Loads the given Markdown text to the view as rich formatted HTML.
-	 * @param markdownTextId Markdown text id from String resource.
-	 */
-	public void loadMarkdown(@StringRes int markdownTextId)
-	{
-		loadMarkdown(getResources().getString(markdownTextId));
-	}
+    /**
+     * Loads the CSS text from Android assets.
+     * @param cssPath CSS file path in assets directory. (ex. "basic_theme.css", "sample/classic.css")
+     */
+    fun loadCssFromAssets(cssPath: String) {
+        this.loadCss(this.readFileFromAsset(cssPath))
+    }
 
-	/**
-	 * Loads the given Markdown text from Android assets.
-	 * @param markdownPath Markdown text file in assets directory. (ex. "hello.md")
-	 */
-	public void loadMarkdownFromAssets(String markdownPath)
-	{
-		loadMarkdown(readFileFromAsset(markdownPath));
-	}
+    // A method to refresh markdown text on screen
+    private fun syncMarkdown() {
+        val html = """
+<!DOCTYPE html>
+<html>
+    <head>
+        <style type="text/css">
+${this.cssText}
+        </style>
+    </head>
+    <body>
+${this.markdownProcessor.markdown(this.markdownText)}
+    </body>
+</html>"""
 
-	/**
-	 * Loads the given CSS text to the Markdown text.
-	 * @param cssText input in CSS format
-	 */
-	public void loadCss(String cssText)
-	{
-		this.cssText = cssText;
-		syncMarkdown();
-	}
+        this.loadData(html, "text/html", "UTF-8")
+    }
 
-	/**
-	 * Loads the CSS text from Android assets.
-	 * @param cssPath CSS file path in assets directory. (ex. "basic_theme.css", "sample/classic.css")
-	 */
-	public void loadCssFromAssets(String cssPath)
-	{
-		loadCss(readFileFromAsset(cssPath));
-	}
+    private fun readFileFromAsset(fileName: String): String = try {
+        this.context.assets.open(fileName).bufferedReader().use { reader -> reader.readText() }
+    } catch (ex: Exception) {
+        Log.d(TAG, "Error while reading file from assets", ex)
+        ""
+    }
 
-	// A method to refresh markdown text on screen
-	private void syncMarkdown()
-	{
-		String html = markdownProcessor.markdown(markdownText);
-		if (cssText != null) {
-			html = 	"<style type=\"text/css\">\n" + cssText + "</style>\n" + html;
-		}
-		loadDataWithBaseURL("fake://", html, "text/html", "UTF-8", null);
-	}
-
-	private String readFileFromAsset(String fileName){
-		try {
-			InputStream input = getContext().getAssets().open(fileName);
-			try {
-				BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(input));
-				StringBuilder content = new StringBuilder(input.available());
-				String line;
-				while ((line = bufferedReader.readLine()) != null) {
-					content.append(line);
-					content.append(System.getProperty("line.separator"));
-				}
-				return content.toString();
-			} finally { input.close(); }
-		} catch (Exception ex){
-			Log.d(TAG, "Error while reading file from assets", ex);
-			return null;
-		}
-	}
+    companion object {
+        private const val TAG = "MarkdownView"
+    }
 }
